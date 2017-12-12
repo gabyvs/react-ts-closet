a1486d9f93b853db2f3f2a874dffa95808164bf3
Gaby Vazquez <gabs.vz@gmail.com>
2017-12-11 21:42:37 -0800

    Output management

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = ['Hello', 'webpack', '2'].join(' ');
     element.classList.add('hello');
 
-    // Add the image
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
 
     return element;
 }
