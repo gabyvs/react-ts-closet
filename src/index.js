2967cfe1899f2606b684b44e6c37d43e7d075c90
Gaby Vazquez <gabs.vz@gmail.com>
2017-12-11 21:32:40 -0800

    Adding assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
+import './style.css';
+import Icon from './icon.png';
+
 function component() {
     var element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = ['Hello', 'webpack', '2'].join(' ');
+    element.classList.add('hello');
+
+    // Add the image
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
