74857ae3e09b33b3142c0a77fe66a0a30b7bcd4c
Gaby Vazquez <gabs.vz@gmail.com>
2017-12-12 21:28:55 -0800

    Loading images with react and typescript.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
+import './hello.css';
+const logo = require('./icon.png');
 
 export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+export const Hello = (props: HelloProps) => (
+    <div>
+        <h1 className="hello">Hello from {props.compiler} and {props.framework}!</h1>
+        <img src={String(logo)}/>
+    </div>
+);
