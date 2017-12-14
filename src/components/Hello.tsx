9a3982cd1077bf331c0ed65f0476e8d6dd0c5998
Gaby Vazquez <gabs.vz@gmail.com>
2017-12-13 20:42:14 -0800

    CSS modules.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Test} from "./Test";
+import * as styles from "./hello.css";
 const logo = require('./icon.png');
 import * as jsonData from "./data.json";
 import { Data } from "../domain/domain";
@@ -8,11 +9,13 @@ export interface HelloProps { compiler: string; framework: string; }
 
 export const Hello = (props: HelloProps) => {
     console.log((jsonData as any).data);
+    console.log((styles as any).hello);
 
     const data: Data = (jsonData as any);
+    const st: any = styles;
     console.log(data.closet);
     return (
-        <div className="hello">
+        <div className={st.hello}>
             <h1>Hello from {props.compiler} and {props.framework}!</h1>
             <img src={String(logo)}/>
             <Test />
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,3 +1,6 @@
 import * as React from 'react';
+import * as styles from "./test.css";
 
-export const Test = () => <div className="test"><h1>Should be black!</h1></div>;
+const st: any = styles;
+
+export const Test = () => <div className={st.hello}><h1>Should be black!</h1></div>;
