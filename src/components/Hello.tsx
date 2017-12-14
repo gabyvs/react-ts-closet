318d236b64ba6abd7fbad3f4623c8e47fb3eb007
Gaby Vazquez <gabs.vz@gmail.com>
2017-12-13 19:53:00 -0800

    Ready to start.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
-import './hello.css';
+import {Test} from "./Test";
 const logo = require('./icon.png');
 
 export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = (props: HelloProps) => (
-    <div>
-        <h1 className="hello">Hello from {props.compiler} and {props.framework}!</h1>
-        <img src={String(logo)}/>
-    </div>
-);
+export const Hello = (props: HelloProps) => {
+    return (
+        <div className="hello">
+            <h1>Hello from {props.compiler} and {props.framework}!</h1>
+            <img src={String(logo)}/>
+            <Test />
+        </div>
+    );
+};
---
+++ src/components/Test.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const Test = () => <div className="test"><h1>Should be black!</h1></div>;
